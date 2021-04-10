8b7fe372cc13eb379da7b1653b3fbfe1eb1667aa
Bhawin <Bhawin@192.168.1.102>
2021-04-10 13:55:55 +1200

    Done some code for Distribute Runs and put our name and Id number on each of the files. Created a bufferedWriter for the System.in so it can be read of the java console.

--- compx301as1/src/DistributeRuns.java
+++ compx301as1/src/DistributeRuns.java
@@ -1,4 +1,36 @@
+/**
+ * Name and ID:
+ * Bhawin Mistry 1365187
+ * Reece Brown 1365178
+ */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 
 public class DistributeRuns {
-
+	
+	private int posInt = 0;
+	
+	public DistributeRuns(int posInteger) {
+		if(posInteger > 1) {
+			posInteger = posInt;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		//BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		//String output = reader.readLine();
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\cygwin64\\home\\Bhawin\\301\\tempFile.txt"));
+		bw.write(new InputStreamReader(System.in).toString());
+		bw.close();
+	}
+	
 }
+
+//constructor takes a positive integer greater than 1
+//It should accept standard input, which it expects will be text in the format of the output from your CreateRuns program
--- compx301as1/src/MergeRuns.java
+++ compx301as1/src/MergeRuns.java
@@ -1,3 +1,8 @@
+/**
+ * Name and ID:
+ * Bhawin Mistry 1365187
+ * Reece Brown 1365178
+ */
 
 public class MergeRuns {
 
